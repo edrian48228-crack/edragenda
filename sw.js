8969ae2a65bdea3839d3a4f99bd78bc9a0e102e2
edrian48228-crack <edrian48228@protonmail.com>
2026-06-04 14:43:40 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-agenda-v16-20260604';
+const CACHE = 'mi-agenda-v16-20260604b';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
